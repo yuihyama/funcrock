e97f3bc4b9afbe2829a7d4a9427f19c2902e7c2e
yuihyama <yuihyama@gmail.com>
2018-07-28 17:51:27 +0900

    Update

--- test/add_test.rb
+++ test/add_test.rb
@@ -6,6 +6,7 @@ require './lib/add'
 class AddTest < Minitest::Test
   def test_add
     assert_equal 0, add(0, 0)
+    assert_equal 0, add(-0, -0)
     assert_equal 0, add(-0, 0.0)
     assert_equal 0, add(0.0, 0.0)
     assert_equal 3, add(1, 2)
@@ -15,9 +16,17 @@ class AddTest < Minitest::Test
     assert add(1, 2)
     assert add(-1, -2)
 
-    assert_output("0.0\n") {puts add(0.0, 0.0)}
-    assert_output("0.0\n") {puts add(-0.0, 0.0)}
-    assert_output("3\n") {puts add(1, 2)}
-    assert_output("-3\n") {puts add(-1, -2)}
+    assert_output("0\n") { puts add(0, 0) }
+    assert_output("0\n") { puts add(-0, -0) }
+    assert_output("0\n") { puts add(-0, 0) }
+    assert_output("0\n") { puts add(0, -0) }
+    assert_output("0.0\n") { puts add(0.0, 0.0) }
+    assert_output("0.0\n") { puts add(-0.0, 0.0) }
+    assert_output("0.0\n") { puts add(0.0, -0.0) }
+    assert_output("-0.0\n") { puts add(-0.0, -0.0) }
+    assert_output("3\n") { puts add(1, 2) }
+    assert_output("3.0\n") { puts add(1.0, 2.0) }
+    assert_output("-3\n") { puts add(-1, -2) }
+    assert_output("-3.0\n") { puts add(-1.0, -2.0) }
   end
 end
