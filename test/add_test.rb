919097367e5886f37012a337c27d846864c65618
yuihyama <yuihyama@gmail.com>
2018-07-30 12:47:22 +0900

    Clean

--- test/add_test.rb
+++ test/add_test.rb
@@ -5,6 +5,9 @@ require './lib/add'
 
 class AddTest < Minitest::Test
   def test_add
+    assert add(1, 2)
+    assert add(-1, -2)
+    
     assert_equal 0, add(0, 0)
     assert_equal 0, add(-0, -0)
     assert_equal 0, add(-0, 0.0)
@@ -12,9 +15,6 @@ class AddTest < Minitest::Test
     assert_equal 3, add(1, 2)
     assert_equal 3, add(1.0, 2.0)
     assert_equal -3, add(-1, -2)
-
-    assert add(1, 2)
-    assert add(-1, -2)
 
     assert_output("0\n") { puts add(0, 0) }
     assert_output("0\n") { puts add(-0, -0) }
