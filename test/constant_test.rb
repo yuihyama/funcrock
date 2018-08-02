30c24fb363753ad98d7fcd10687c41033bdba33b
yuihyama <yuihyama@gmail.com>
2018-08-02 12:05:44 +0900

    Add file

---
+++ test/constant_test.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require './lib/constant'
+
+class ConstantTest < Minitest::Test
+  def test_constant
+    assert_equal 0, constant(0)
+    assert_equal 0.0, constant(0)
+    assert_equal -0, constant(-0)
+    assert_equal -0.0, constant(-0.0)
+    assert_equal 1, constant(1)
+
+    assert_output("0\n") { puts constant(0) }
+    assert_output("0.0\n") { puts constant(0.0) }
+    assert_output("0\n") { puts constant(-0) }
+    assert_output("-0.0\n") { puts constant(-0.0) }
+    assert_output("1\n") { puts constant(1) }
+  end
+end
