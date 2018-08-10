f750e9c4d55a94db03c060f9ed3c61ba93fa59a3
yuihyama <yuihyama@gmail.com>
2018-08-10 11:31:30 +0900

    Add file

---
+++ test/lt_test.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require './lib/lt'
+
+class LTTest < Minitest::Test
+  def test_lt
+    assert_equal true, lt(1, 2)
+    assert_equal false, lt(2, 1)
+    assert_equal true, lt(1.0, 2)
+    assert_equal false, lt(2, 1.0)
+    assert_equal true, lt(1.1, 2.2)
+    assert_equal false, lt(2.2, 1.1)
+    assert_equal false, lt(0, 0)
+    assert_equal false, lt(0.0, 0.0)
+    assert_equal true, lt(-1, 2)
+    assert_equal false, lt(1, -2)
+    assert_equal true, lt(-1.1, 2.2)
+    assert_equal false, lt(1.1, -2.2)
+    assert_equal false, lt(-1.1, -2.2)
+    assert_equal true, lt(-2.2, 1.1)
+    assert_equal false, lt(2.2, -1.1)
+    assert_equal true, lt(-2.2, -1.1)
+
+    assert_output("true\n") { p lt(1, 2) }
+    assert_output("true\n") { puts lt(1, 2) }
+    assert_output("false\n") { puts lt(2, 1) }
+    assert_output("false\n") { puts lt(0, 0) }
+    assert_output("false\n") { puts lt(2.0, 1) }
+    assert_output("true\n") { puts lt(-2, 1.0) }
+    assert_output("false\n") { puts lt(2.2, -1) }
+    assert_output("true\n") { puts lt(-2, -1) }
+  end
+end
