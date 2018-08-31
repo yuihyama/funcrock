c28d42e32a1cc20ddc5d8f8116325068d657b13e
yuihyama <yuihyama@gmail.com>
2018-08-31 17:28:16 +0900

    Update

--- test/all-test/all_test.rb
+++ test/all-test/all_test.rb
@@ -27,12 +27,24 @@ end
 
 class AllTest < Minitest::Test
   def test_all
+    # assert_equal:
     assert_equal 10, add(add(add(1, 2), 3), 4)
     assert_equal 9, sub(add(add(add(1, 2), 3), 4), 1)
     assert_equal 18, mul(sub(add(add(add(1, 2), 3), 4), 1), 2)
+    assert_equal 9, div(mul(sub(add(add(add(1, 2), 3), 4), 1), 2), 2)
+    #
+    res1 = div(mul(sub(add(add(add(1, 2), 3), 4), 1), 2), 2)
+    assert_equal 9, res1
+    #
 
+    # assert_output:
     assert_output("10\n") { puts add(add(add(1, 2), 3), 4) }
     assert_output("9\n") { puts sub(add(add(add(1, 2), 3), 4), 1) }
     assert_output("18\n") { puts mul(sub(add(add(add(1, 2), 3), 4), 1), 2) }
+    assert_output("9\n") { puts div(mul(sub(add(add(add(1, 2), 3), 4), 1), 2), 2) }
+    #
+    res1 = div(mul(sub(add(add(add(1, 2), 3), 4), 1), 2), 2)
+    assert_output("9\n") { puts res1 }
+    #
   end
 end
