6ba0c0f1864142031c994798039478e82d96ed01
yuihyama <yuihyama@gmail.com>
2018-08-31 16:59:02 +0900

    Update

--- test/all-test/all_test.rb
+++ test/all-test/all_test.rb
@@ -1,10 +1,33 @@
 #!/usr/bin/env ruby
 
 require 'minitest/autorun'
-require './lib/add'
+
+# require './lib/*.rb':
+#
+Dir[File.expand_path('../../../lib', __FILE__) << '/*.rb'].each do |file|
+  require file
+end
+# $ cd funcrock
+# $ ruby test/all-test/all_test.rb
+# or
+# cd funcrock/test/all-test
+# ruby all_test.rb
+
+# one file:
+#
+# require './lib/add'
+# or
+# require_relative '../../lib/add'
+# $ cd funcrock
+# $ ruby test/all-test/all_test.rb
+#
+# require '../../lib/add'
+# $ cd funcrock/test/all-test
+# $ ruby all_test.rb
 
 class AllTest < Minitest::Test
   def test_all
     assert_equal 10, add(add(add(1, 2), 3), 4)
+    assert_equal 9, sub(add(add(add(1, 2), 3), 4), 1)
   end
 end
