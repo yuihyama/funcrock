8732ee21c3e49a128f90672a4af972e91118ae05
yuihyama <yuihyama@gmail.com>
2018-09-01 10:15:50 +0900

    Add file

---
+++ test/antenum_test.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require './lib/antenum'
+
+class AntenumTest < Minitest::Test
+  def test_antenum
+    assert_equal 0, antenum(1)
+    assert_equal(-1, antenum(0))
+    assert_equal(-2, antenum(-1))
+    assert_equal 1.0999999999999999, antenum(1.2)
+    assert_equal 1.0, antenum(1.1)
+    assert_equal 0.9, antenum(1.0)
+    assert_equal(-1.3, antenum(-1.2))
+    assert_equal(-1.0, antenum(-0.9))
+
+    assert_output("0\n") { p antenum(1) }
+    assert_output("0\n") { puts antenum(1) }
+    assert_output("1.0999999999999999\n") { puts antenum(1.2) }
+    assert_output("-1.3\n") { puts antenum(-1.2) }
+  end
+end
