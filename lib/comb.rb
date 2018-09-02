5c218fe83c1e18b452e8b75f746f72f46e50004a
yuihyama <yuihyama@gmail.com>
2018-09-02 12:42:19 +0900

    Add file

---
+++ lib/comb.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require './lib/factorial'
+
+def comb(n, r)
+  f_n = n.to_f
+  f_r = r.to_f
+  return 0 if f_n < 0 || f_r < 0
+  return 0 if f_n == 0 && f_r == 1
+  return 1 if f_n == f_r
+  factorial(f_n) / (factorial(f_r) * factorial(f_n - f_r))
+end
