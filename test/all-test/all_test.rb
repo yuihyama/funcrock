7fcde956bbd1a27b60d4b5113612eb21a1e836f3
yuihyama <yuihyama@gmail.com>
2018-09-02 22:26:40 +0900

    Update

--- test/all-test/all_test.rb
+++ test/all-test/all_test.rb
@@ -53,6 +53,9 @@ class AllTest < Minitest::Test
     assert_equal 1, nextnum(antenum(1))
     assert_equal 1440, add(perm(10, 3), perm(10, 3))
     assert_equal 240, add(comb(10, 3), comb(10, 3))
+    assert_equal [8, 9, 10], sort([add(1, 9), add(1, 8), add(1, 7)])
+    assert_equal [8, 9, 10], spsort(add(1, 9), add(1, 8), add(1, 7))
+    assert_equal [1, 2, 4, 5, 10, 20], divisors(mul(add(1, 9), 2))
 
     # assert_output: p:
     assert_output("10\n") { p add(add(add(1, 2), 3), 4) }
@@ -85,6 +88,9 @@ class AllTest < Minitest::Test
     assert_output("1\n") { p nextnum(antenum(1)) }
     assert_output("1440\n") { p add(perm(10, 3), perm(10, 3)) }
     assert_output("240\n") { p add(comb(10, 3), comb(10, 3)) }
+    assert_output("[8, 9, 10]\n") { p sort([add(1, 9), add(1, 8), add(1, 7)]) }
+    assert_output("[8, 9, 10]\n") { p spsort(add(1, 9), add(1, 8), add(1, 7)) }
+    assert_output("[1, 2, 4, 5, 10, 20]\n") { p divisors(mul(add(1, 9), 2)) }
 
     # assert_output: puts:
     assert_output("10\n") { puts add(add(add(1, 2), 3), 4) }
@@ -106,14 +112,19 @@ class AllTest < Minitest::Test
     assert_output("true\n") { puts le(constant(1), constant(1.0)) }
     assert_output("false\n") { puts lt(constant(1), constant(1.0)) }
     assert_output("2\n4\n6\n8\n10\n12\n14\n16\n18\n") {
-      puts mulseq(add(1, 0), add(1, 8), 2) }
+      puts mulseq(add(1, 0), add(1, 8), 2)
+    }
     assert_output("2\n4\n6\n8\n10\n12\n14\n16\n18\n") {
-      puts mulseq1 }
+      puts mulseq1
+    }
     assert_output("2\n") { puts factorial(nextnum(1)) }
     assert_output("56\n") { puts nextnum(spadd(1, 10)) }
     assert_output("1\n") { puts nextnum(antenum(1)) }
     assert_output("1440\n") { puts add(perm(10, 3), perm(10, 3)) }
     assert_output("240\n") { puts add(comb(10, 3), comb(10, 3)) }
+    assert_output("8\n9\n10\n") { puts sort([add(1, 9), add(1, 8), add(1, 7)]) }
+    assert_output("8\n9\n10\n") { puts spsort(add(1, 9), add(1, 8), add(1, 7)) }
+    assert_output("1\n2\n4\n5\n10\n20\n") { puts divisors(mul(add(1, 9), 2)) }
 
     # assert_output: print:
     assert_output("9\n") {
