5fd4d14e3645eaaeeac94d53c16b92d09d8c776a
yuihyama <yuihyama@gmail.com>
2018-09-03 18:49:07 +0900

    Add file

---
+++ test/println_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require './lib/println'
+
+class PrintlnTest < Minitest::Test
+  def test_println
+    assert_nil println(1)
+    assert_nil println(1, 2)
+    assert_nil println(1, "\n", 2)
+    assert_nil println(1 + 2)
+
+    assert_output("1\nnil\n") { p println(1) }
+    assert_output("1 2\nnil\n") { p println(1, 2) }
+    assert_output("1 \n 2\nnil\n") { p println(1, "\n", 2) }
+    assert_output("3\nnil\n") { p println(1 + 2) }
+    assert_output("6.0\nnil\n") { p println(1 + 2 + 3.0) }
+  end
+end
