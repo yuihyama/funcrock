7621b68b4112a14946860aea5c3bbc137979074e
yuihyama <yuihyama@gmail.com>
2018-09-03 22:55:53 +0900

    Clean

--- lib/comb.rb
+++ lib/comb.rb
@@ -3,8 +3,10 @@ require './lib/factorial'
 def comb(n, r)
   f_n = n.to_f
   f_r = r.to_f
+
   return 0 if f_n < 0 || f_r < 0
   return 0 if f_n == 0 && f_r == 1
   return 1 if f_n == f_r
+
   factorial(f_n) / (factorial(f_r) * factorial(f_n - f_r))
 end
