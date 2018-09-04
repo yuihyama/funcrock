bb887f456908095076974f3ef2daea90cb00dab0
yuihyama <yuihyama@gmail.com>
2018-09-04 19:01:50 +0900

    Add file

---
+++ test/pos_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require './lib/pos'
+
+class PosTest < Minitest::Test
+  def test_pos
+    assert pos(1)
+    assert_equal 0, pos(0)
+    assert_equal(-0, pos(0))
+    assert_equal 1, pos(1)
+    assert_equal 1.1, pos(1.1)
+    assert_equal(-1.1, pos(-1.1))
+    assert_equal 0.01, pos(0.01)
+    assert_equal(-0.01, pos(-0.01))
+
+    assert_output("0\n") { p pos(0)}
+    assert_output("0\n") { puts pos(0) }
+    assert_output("1\n") { puts pos(1) }
+    assert_output("1.1\n") { puts pos(1.1) }
+    assert_output("-1.1\n") { puts pos(-1.1) }
+    assert_output("0.01\n") { puts pos(0.01) }
+    assert_output("-0.01\n") { puts pos(-0.01) }
+  end
+end
