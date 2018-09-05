f8d31a44ff07a3751f729bda78ad1e088acf5b6b
yuihyama <yuihyama@gmail.com>
2018-09-06 04:32:24 +0900

    Add file

---
+++ examples/randprime_example.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require './lib/randprime'
+
+# primerange(10) # => [2, 3, 5, 7]
+puts randprime(10) # => 2
+puts randprime(10) # => 3
+puts randprime(10) # => 5
+puts randprime(10) # => 7
+puts
+
+5.times { puts randprime(10) }
+# =>
+# 7
+# 2
+# 3
+# 3
+# 5
+# Output: Random!!
