419cf3a563e894d2179590c8f3deb8a70cdf6676
yuihyama <yuihyama@gmail.com>
2018-09-10 03:01:24 +0900

    Add file

---
+++ test/sum_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require './lib/sum'
+
+class SumTest < Minitest::Test
+  def test_sum
+    assert_equal 6, sum([1, 2, 3])
+    assert_equal 4, sum([-1, 2, 3])
+    assert_equal 2, sum([1, -2, 3.0])
+    assert_equal 2.0, sum([1, -2, 3.0])
+    assert_equal 'abc', sum(['a', 'b', 'c'])
+
+    assert_output("6\n") { p sum([1, 2, 3]) }
+    assert_output("2.0\n") { puts sum([1, -2, 3.0]) }
+    assert_output("\"abc\"\n") { p sum(['a', 'b', 'c']) }
+    assert_output("abc\n") { puts sum(['a', 'b', 'c']) }
+    assert_output("abc\n") { print sum(['a', 'b', 'c']), "\n" }
+  end
+end
