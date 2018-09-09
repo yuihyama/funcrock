2e5af840e4326b9b38144f51eccf3989bb6bd2a0
yuihyama <yuihyama@gmail.com>
2018-09-10 03:02:18 +0900

    Add file

---
+++ benchmark/sum_benchmark.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'benchmark'
+require './lib/sum'
+
+Benchmark.bmbm do |x|
+  x.report('sum([1, 2, 3])') { sum([1, 2, 3]) }
+  x.report("sum(['a', 'b', 'c'])") { sum(['a', 'b', 'c']) }
+
+  x.report('300000.times { sum([1, 2, 3]) }') {
+    300000.times { sum([1, 2, 3]) }
+  }
+  x.report("300000.times { sum(['a', 'b', 'c']) }" ) {
+    300000.times { sum(['a', 'b', 'c']) }
+  }
+end
